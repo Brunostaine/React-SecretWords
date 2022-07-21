6c12c9b2661e04eeaece4b4d896743889fee02d8
Bruno Staine <brunostaine@hotmail.com>
2022-07-21 18:46:03 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 
+import StartScreen from './components/StartScreen';
+
+import {useCallback, useEffect, useState} from 'react'
+import {wordsList } from './data/Words'
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+    {id: 1, name: "start"},
+    {id: 2, name: "game"},
+    {id: 3, name: "end"}
+]
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [gameStage, setGameStage] = useState(stages[0].name)
+    const [words] = useState(wordsList)
+
+    const [pickedWord, setPickedWord] = useState("")
+    const [pickedCategory, setPickedCategory] = useState("")
+    const [letters, setLetters] = useState([])
+
+    const pickWordAndPickCategory = () => {
+        const categories = Object.keys(words);
+        const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+        console.log(category)
+
+        const word = words[category][Math.floor(Math.random() * words[category].length)]
+        console.log(word)
+
+        return {word, category}
+    }
+
+    const startGame = () =>{
+        const {word, category} = pickWordAndPickCategory()
+
+        let wordLetters = word.split("")
+        wordLetters = wordLetters.map((l) => l.toLowerCase())
+
+        console.log(word, category)
+        console.log(wordLetters)
+
+        setPickedWord(word)
+        setPickedCategory(category)
+        setLetters(letters)
+        setGameStage(stages[1].name)
+    }
+
+    const verifyLetter = () => {
+        setGameStage(stages[2].name)
+    }
+
+    const retry = () => {
+        setGameStage(stages[0].name)
+    }
+    
+    return (
+        <div className="App">
+            {gameStage === "start" && <StartScreen startGame={startGame}/>}
+            {gameStage === "game" && <Game verifyLetter={verifyLetter}/>}
+            {gameStage === "end" && <GameOver retry={retry}/>}
+        </div>
+    );
 }
 
 export default App;
